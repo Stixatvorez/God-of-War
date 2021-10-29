d2c86c7cf7081008bad6c40a960bef8220bc2aa7
opilane <opilane@HP-CZC6297B8F>
2021-10-29 08:56:14 +0300

    Rock Paper Scissors

--- God_of_War.py
+++ God_of_War.py
@@ -1,128 +1,48 @@
-print("Rock Paper Scissors")
-#constants
-Rock = 1
-Paper = 2
-Scissors = 3
-
-#Define the main function
-def main():
-
-    #set control loop
-    keep_going = 'Y'
 
 
-    #set counter to zero
-    computer_wins = 0
-    player_wins = 0
-    tie_score = 0
-
-    #call display message
-    display_message()
-
-    while keep_going == 'y' or keep_going == 'Y':
-
-        play_game()
+import random
 
 
-        #prompt user to keep going
-        keep_going = input('more? (Y for Yes): ')
+start = input('Вы запустили игру "Камень, ножницы, бумага". Хотите поиграть? (Вводите + или -): ')
 
-    print('computer won', computer_wins, 'times')
-    print('player won', player_wins, 'times')
-    print('There were', tie_score, 'tie scores')
+if start == '+':
+    print('Загрузка...')
+    print("Загрузка завершена! Начинаем!")
+    print("3...2...1...")
+    print('Если захотите закончить вводите "-".')
+    print('Если захотите узнать счёт вводите "с".')
+    user_ball = 0
+    rand_ball = 0
+    while True:
+        user = input("Камень, ножницы или бумага? (Вводите к, н или б): ")
+        list_play = ['к', 'н', 'б']
+        if user in list_play:
+            rand = random.choice(list_play)
+            print(rand)
 
-def play_game():
-
-    #get random input
-    computer = get_random()
-
-    #get the players input
-    play = get_play()
-
-    #validate input
-    if play == '1' or play == '2' or play == '3':
-        play == True
-    else:
-        play == False
-        print('Error: Invalid Entry')
-        play = input('Please enter 1 for Rock, 2 for Paper, or 3 for Scissors: ')
-
-    if play == computer:
-        print('Tie Score, Please try again')
-        tie_score += 1
-
-    else:
-        get_score(computer, play)
-
-    print('The computer chose:', computer)
-    print('The player chose: ', play)
-
-
-#define display message
-def display_message():
-    print('Welcome to Rock Paper Scissors, a game of chance to see who will')
-    print('outsmart the other. This game is Man VS Computer.')
-    print('The program will select a random integer and then ask you for an integer')
-    print('1 for Rock 2 for paper or 3 for Scissors. The program will then tell')
-    print('you who won the game.')
-    print('GOOD LUCK!')
-    print
-    print
-
-def get_random():
-    import random
-
-    #generate random int
-    computer = random.randint(1, 3)
-    return computer
+            if rand == 'к' and user == 'н':
+                rand_ball += 1
+            if rand == 'к' and user == 'б':
+                user_ball += 1
+            if rand == 'н' and user == 'к':
+                user_ball += 1
+            if rand == 'н' and user == 'б':
+                rand_ball += 1
+            if rand == 'б' and user == 'н':
+                user_ball += 1
+            if rand == 'б' and user == 'к':
+                rand_ball += 1
+        elif user == 'с':
+            print('Ваши баллы - ', user_ball, '. Баллы вашего соперника - ', rand_ball, ".")
+        elif user == '-':
+            print('Ваши баллы - ', user_ball, '. Баллы вашего соперника - ', rand_ball, ".")
+            print('Конец игры! Заходите ещё!')
+            break
+        else:
+            print('Вводите к, н или б')
 
 
-def get_play():
-    #prompt user to enter an integer 1, 2, or 3
-    play = input('Select 1 for Rock, 2 for Paper, or 3 for Scissors: ')
-    return play
-
-def get_score(computer, play):
-
-    if computer == 1 and play == 2:
-        score = 'player wins'
-        print('Paper covers Rock, Player Wins')
-        #player wins
-        player_wins += 1
-
-    elif computer == 1 and play == 3:
-        score = 'computer wins'
-        print('Scissors cut Paper, Computer Wins')
-        #computer wins
-        computer_wins += 1
-
-
-    elif computer == 2 and play == 1:
-        score = 'computer wins'
-        print('Paper covers Rock, Computer Wins')
-        #computer wins
-        computer_wins += 1
-
-    elif computer == 2 and play == 3:
-        score = 'player wins'
-        print('Scissors cut Paper, Player Wins')
-
-        #player wins
-        player_wins += 1
-
-
-    elif computer == 3 and play == 1:
-        score = 'player wins'
-        print('Rock smashes Scissors, Player Wins')
-        #player wins
-        player_wins += 1
-
-    elif computer == 3 and play == 2:
-        score = 'computer wins'
-        print('Scissors cut Paper, Computer Wins')
-        #computer wins
-        computer_wins += 1
-
-
-#call main function
-main()
+if start == '-':
+    print('Жаль... :(')
+else:
+    print('Простите, я вас не понял, если хотите играть перезапустите программу и введите "+". Спасибо!')
